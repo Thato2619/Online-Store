6247101e6ca34dfa9264004bd1526a2011ea83d7
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-28 14:35:50 +0200

    add cart icon

--- index.php
+++ index.php
@@ -39,11 +39,12 @@
           <a class="nav-link" href="#">Contact</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Cart</a>
+          <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">login</a>
+          <a class="nav-link" href="#">Total Price:100 -+</a>
         </li>
+      </ul>
         
         
 
