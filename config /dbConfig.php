ed499bd5f338da70f2d6220b051482a9c64b56cb
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-29 09:11:06 +0200

    create dbConfig file that connects to the database
    put dbConfig file in the config folder

---
+++ config /dbConfig.php
@@ -0,0 +1,22 @@
+<?php 
+
+//create variables to of the database
+$serverName = 'localhost';
+$userName = 'root';
+$password = '';
+$dbName = 'Online-Store';
+
+
+//create connection in database
+$connection = mysqli_connect($serverName, $userName, $password, $dbName);
+
+//check if connection is successful
+if(mysqli_connect_errno()){
+    echo "Failed to connect!";
+    exit();
+}
+echo "Connection succcessful!"
+
+
+
+?>
