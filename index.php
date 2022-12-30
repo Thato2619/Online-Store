c24d0ccd5aa7e7cba595c995e74c513bc2ea21b6
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-30 12:49:03 +0200

    indent  whole code

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 
 require_once './config/dbConfig.php';
@@ -105,73 +105,76 @@ require_once './config/dbConfig.php';
 
   </div>
 
-<!-- Add Title -->
-<div class="bg-light">
-  <h1 class="display-1">WELCOME TO GLISTENING GLOW</h1>
-</div>
+  <!-- Add Title -->
+  <div class="bg-light">
+    <h1 class="display-1">WELCOME TO GLISTENING GLOW</h1>
+  </div>
 
   <section>
-  <div class="row">
-    <?php 
-    #create selected-query variables that contains selected from products from the database
-    $selected_query = "Select * from `Products`"
-    
-    ?>
-    <div class="col-md-10 md-4" >
-      <!-- Type of facial products-->
-      <div class="row">
-        <!-- card of Ala-Rasi-->
-        <div class="col-md-4  mb-4">
-          <div class="card">
-            <img src="./static //Images//Ala-Rasi.jpeg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Ala-Rasi</h5>
-              <p class="card-text">Hydrating, with Vitamin C</p>
-              <p class="card-text">R80</p>
-              <a href="#" class="btn btn-success1">ADD TO CART</a>
-              <a href="#" class="btn btn-success">VIEW MORE</a>
+    <div class="row">
+      <?php
+      #create selected-query variables that contains selected from products from the database
+      $selected_query = "Select * from `Products`";
+      # write out the result_query varibale tht contains mysqli_query
+      $result_query = mysqli_query($connection, $selected_query);
+      $row = mysqli_fetch_assoc($result_query);
+      echo $row['product_id'];
+      ?>
+      <div class="col-md-10 md-4">
+        <!-- Type of facial products-->
+        <div class="row">
+          <!-- card of Ala-Rasi-->
+          <div class="col-md-4  mb-4">
+            <div class="card">
+              <img src="./static //Images//Ala-Rasi.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Ala-Rasi</h5>
+                <p class="card-text">Hydrating, with Vitamin C</p>
+                <p class="card-text">R80</p>
+                <a href="#" class="btn btn-success1">ADD TO CART</a>
+                <a href="#" class="btn btn-success">VIEW MORE</a>
+              </div>
             </div>
           </div>
-        </div>
-        
 
-        <!-- card of Eudaimonia -->
-        <div class="col-md-4  mb-4">
-          <div class="card">
-            <img src="./static //Images//Eudaimonia.jpeg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Eudaimonia</h5>
-              <p class="card-text">Removes impuritiess, contains Salicylic Acid</p>
-              <p class="card-text">R120</p>
-              <a href="#" class="btn btn-success1">ADD TO CART</a>
-              <a href="#" class="btn btn-success">VIEW MORE</a>
+
+          <!-- card of Eudaimonia -->
+          <div class="col-md-4  mb-4">
+            <div class="card">
+              <img src="./static //Images//Eudaimonia.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Eudaimonia</h5>
+                <p class="card-text">Removes impuritiess, contains Salicylic Acid</p>
+                <p class="card-text">R120</p>
+                <a href="#" class="btn btn-success1">ADD TO CART</a>
+                <a href="#" class="btn btn-success">VIEW MORE</a>
+              </div>
             </div>
           </div>
-        </div>
 
-         <!-- card of Hygge-->
-        <div class="col-md-4  mb-4">
-          <div class="card">
-            <img src="./static //Images//Hygge.jpeg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Hygge</h5>
-              <p class="card-text">Brightening, contains Hyaluronic Acid </p>
-              <p class="card-text">R100</p>
-              <a href="#" class="btn btn-success1">ADD TO CART</a>
-              <a href="#" class="btn btn-success">VIEW MORE</a>
+          <!-- card of Hygge-->
+          <div class="col-md-4  mb-4">
+            <div class="card">
+              <img src="./static //Images//Hygge.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Hygge</h5>
+                <p class="card-text">Brightening, contains Hyaluronic Acid </p>
+                <p class="card-text">R100</p>
+                <a href="#" class="btn btn-success1">ADD TO CART</a>
+                <a href="#" class="btn btn-success">VIEW MORE</a>
+              </div>
             </div>
           </div>
-        </div>
 
-        
+
 
   </section>
-  
 
 
 
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
