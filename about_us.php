f6c1a7c8553b578bc5ae67b107ecac80cd3b4a84
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-01-03 17:43:27 +0200

    remove h5 and two p tags

--- about_us.php
+++ about_us.php
@@ -74,20 +74,9 @@
   <!-- slideshow of models and products -->
   
 
-  
-
-  <br>
-
-  <br>
-
-  <br>
-
   <div class="card text-bg-dark">
   <img src="./DBImages/product_images/pexels-angela-roma-7479917.jpg" class="card-img" alt="...">
   <div class="card-img-overlay">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small>Last updated 3 mins ago</small></p>
   </div>
 </div>
 
