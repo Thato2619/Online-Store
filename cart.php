52ac6b109c2563d5c9369e84132ee9c4e90bdec7
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-01-12 06:16:58 +0200

    create a query variable that contains info from database
    - create result variable that contains mysqli_query that contains connection and query as argument
    
    - create conditional statement

--- cart.php
+++ cart.php
@@ -56,6 +56,15 @@ $connection = mysqli_connect($serverName, $userName, $password, $dbName);
 
 <div class="container" style="width: 65%">
     <h1 class="display-1">SHOPPING CART</h1>
+    <?php 
+    $query = "SELECT * FROM `Products` ORDER BY id ASC";
+    $result = mysqli_query($connection, $query);
+    if(mysqli_num_rows($result) > 0){
+        
+    }
+    
+    
+    ?>
   </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
