8b6fff0029e02b551dc2b5979a4275ddbea94e85
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-01-12 05:45:11 +0200

    create php tag with session_start()

--- cart.php
+++ cart.php
@@ -1,3 +1,7 @@
+<?php 
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,6 +19,7 @@
     <title>Shopping cart</title>
 </head>
 <body>
+    <div></div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
