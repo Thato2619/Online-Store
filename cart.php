1cea5a5191554dae9fff46f4abe546063b3c8489
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-01-12 06:08:42 +0200

    add navbar and heading (name it shopping cart)

--- cart.php
+++ cart.php
@@ -27,7 +27,36 @@ $connection = mysqli_connect($serverName, $userName, $password, $dbName);
     <title>Shopping cart</title>
 </head>
 <body>
-    <div></div>
+      <!-- Navbar-->
+  <nav class="navbar navbar-expand-lg nav-colour">
+    <div class="container-fluid">
+      <img src="./static /Images/online_store_logo.png" alt="" class="store_logo">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="view_more.php">Shop</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="gallery.php">Gallery</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about_us.php">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+        </ul>
+</nav>
+
+<div class="container" style="width: 65%">
+    <h1 class="display-1">SHOPPING CART</h1>
+  </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
