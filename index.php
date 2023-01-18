c8faa7ff90855a41bcf3f15dcc7370bb76426f9f
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-01-18 18:01:56 +0200

    in place of button tags, add input tag as replacement
    - create search_product.php

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ r
         <form class="d-flex" role="search" action="/model/search.bar.php" method="get">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <!--<button class="btn btn-outline-success" type="submit">Search</button>-->
-          
+          <input type="submit" value="search" class="btn btn-outline-success">
         </form>
       </div>
     </div>
