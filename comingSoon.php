a4b4565d1ceb514ebb7f208bd75344530265012f
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-01-24 14:26:09 +0200

    add 3 images in carousel that shows case the new product coming soon
    - sheet mask

--- comingSoon.php
+++ comingSoon.php
@@ -72,23 +72,43 @@ include ("/model/cart.php");
     </div>
   </nav>
 
+  <h1 class="h1 fw-bold mb-0 " style="color:#bd1e1e; text-align: center;" >COMING SOON COMING SOON COMING SOON COMING SOON </h1>
 
-  <!-- Jumbotron -->
-<div class="p-5 text-center bg-image rounded-3" style="
-    background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp');
-    height: 400px;
-  ">
-  <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
-    <div class="d-flex justify-content-center align-items-center h-100">
-      <div class="text-white">
-        <h1 class="mb-3">Heading</h1>
-        <h4 class="mb-3">Subheading</h4>
-        <a class="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
+    <!-- slideshow of models and products -->
+    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="10000">
+        <img src="./DBImages/product_images/sheet_mask_2.jpeg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+        </div>
+      </div>
+      <div class="carousel-item" data-bs-interval="2000">
+        <img src="./DBImages/product_images/Coming_soon.jpeg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="./DBImages/product_images/Coming_soon_2.jpeg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+        </div>
       </div>
     </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
-</div>
-<!-- Jumbotron -->
+  </div>
+  <h1 class="h1 fw-bold mb-0 " style="color:#bd1e1e; text-align: center;" > COMING SOON COMING SOON COMING SOON COMING SOON</h1>
 
   <!-- Footer -->
 <footer class="text-center text-lg-start bg-light text-muted" style="background-color: #95a78d !important; color:#fbfbf2 !important;">
