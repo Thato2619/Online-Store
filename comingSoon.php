f7f1cf954d0053eea972f4cd8b4839cd1066e3d0
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-01-24 12:22:13 +0200

    add navbar and footer in new comingSoon.php file
    - add jumbotron
    -add 7 images in the DBImages folder

---
+++ comingSoon.php
@@ -0,0 +1,218 @@
+<?php
+
+require_once __DIR__ . "/config2/dbConfig.php";
+require_once __DIR__ . "/model/cart.php";
+include ("/model/cart.php");
+//include ("model/check_login.php");
+//$user_data = check_login($connection);
+//$_SESSION;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./static /CSS/styles.css">
+</head>
+<title>Glistening Glow</title>
+<!-- Bootsrap Link--->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+<!-- css file link--->
+<link rel="stylesheet" href="./static //CSS//styles.css">
+
+<!-- Font Awesome Link-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+  <!-- Navbar-->
+  <nav class="navbar navbar-expand-lg nav-colour">
+    <div class="container-fluid">
+      <img src="./static /Images/online_store_logo.png" alt="" class="store_logo">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="view_more.php">Shop</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="gallery.php">Gallery</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about_us.php">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="comingSoon.php">COMING SOON</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="checkout.php"><i class="fa-solid fa-cart-shopping"></i><sup><?php number_of_cart_items(); ?></sup></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Total Price:<?php totalPrice(); ?></a>
+          </li>
+        </ul>
+        <form class="d-flex" action="search_product.php" method="get">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_data">
+          <!--<button class="btn btn-outline-success" type="submit">Search</button>-->
+          <input type="submit" value="search" class="btn btn-outline-success" name="search_data_product">
+        </form>
+      </div>
+    </div>
+  </nav>
+
+
+  <!-- Jumbotron -->
+<div class="p-5 text-center bg-image rounded-3" style="
+    background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp');
+    height: 400px;
+  ">
+  <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+    <div class="d-flex justify-content-center align-items-center h-100">
+      <div class="text-white">
+        <h1 class="mb-3">Heading</h1>
+        <h4 class="mb-3">Subheading</h4>
+        <a class="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Jumbotron -->
+
+  <!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted" style="background-color: #95a78d !important; color:#fbfbf2 !important;">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Glistening Glow
+          </h6>
+          <p>
+            Where your skin is intentionally cared for in every sort of way. 
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Our Products
+          </h6>
+          <p>
+            <a href="cart.php" class="text-reset">Ala-Rasi</a>
+          </p>
+          <p>
+            <a href="cart.php" class="text-reset">Eudaimonia</a>
+          </p>
+          <p>
+            <a href="cart.php" class="text-reset">Hygge</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            More Info
+          </h6>
+          <p>
+            <a href="view_more.php" class="text-reset">Pricing</a>
+          </p>
+          <p>
+            <a href="about.php" class="text-reset">About Us</a>
+          </p>
+          <p>
+            <a href="cart.php" class="text-reset">Orders</a>
+          </p>
+          <p>
+            <a href="contact.php" class="text-reset">Help</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i> Cape Town,Century City, SA</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            glisteningGlowInfo@gmail.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> +21 234 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2023 Copyright:
+    <a class="text-reset fw-bold" href="index.php">GlistenGlow.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
